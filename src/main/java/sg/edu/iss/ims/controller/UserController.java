fb674b8c40f965f34b2a12654daec5e19d770179
Cai Yaolong Derek <caiylderek@gmail.com>
2020-12-10 23:46:14 +0800

    Added alert class and alert functionality.
    To use, inject RedirectAttributes into your method function and a
    flashAttribute to key "alert" with the value of a new Alert class

--- src/main/java/sg/edu/iss/ims/controller/UserController.java
+++ src/main/java/sg/edu/iss/ims/controller/UserController.java
@@ -3,12 +3,13 @@ package sg.edu.iss.ims.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import sg.edu.iss.ims.model.Alert;
 import sg.edu.iss.ims.model.User;
 import sg.edu.iss.ims.service.UserImplementation;
 import sg.edu.iss.ims.service.UserInterface;
@@ -29,11 +30,12 @@ public class UserController {
 	}
 	
 	@PostMapping("/login")
-	public String loginAccount(Model model, User user, HttpServletRequest request) {
+	public String loginAccount(Model model, User user, HttpServletRequest request, RedirectAttributes redirAttr) {
 		// Note, user is not currently being passed to this action properly
 		HttpSession session = request.getSession();
 		if (session.getAttribute("user") == null && uService.authenticate(user)) {
-			session.setAttribute("user", user);	
+			session.setAttribute("user", user);
+			redirAttr.addFlashAttribute("alert", new Alert("primary", "Successfully logged in!"));
 		}
 		return "redirect:/";
 	}	
@@ -45,12 +47,12 @@ public class UserController {
 	}
 
 	@PostMapping("/register")
-	public String createAccount(Model model, User user, HttpServletRequest request) {
+	public String createAccount(Model model, User user, HttpServletRequest request, RedirectAttributes redirAttr) {
 		HttpSession session = request.getSession();
 		if (session.getAttribute("user") == null && uService.readUser(user.getUsername()) == null) {
 			uService.createUser(user);
-			
-			session.setAttribute("user", user);	
+			session.setAttribute("user", user);
+			redirAttr.addFlashAttribute("alert", new Alert("primary", "Successfully registered!"));
 		}
 		return "redirect:/";
 	}
---
+++ src/main/java/sg/edu/iss/ims/model/Alert.java
@@ -0,0 +1,36 @@
+package sg.edu.iss.ims.model;
+
+import java.util.Arrays;
+
+public class Alert {
+	private String type;
+	private String message;
+	
+	public Alert(String type, String message) {
+		String[] validTypes = {"primary", "secondary", "success", "danger", "warning", "info", "light", "dark"};
+		if (! Arrays.stream(validTypes).anyMatch(type::equals)) {
+			System.out.println("Your alert type is invalid");
+		} else {
+			this.type = type;
+			this.message = message;
+		}
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
