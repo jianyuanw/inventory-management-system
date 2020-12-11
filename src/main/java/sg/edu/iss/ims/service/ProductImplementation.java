c2bb5728ff04825ea661956a0fcb569ab839e102
MokZF404 <54626382+MokZF404@users.noreply.github.com>
2020-12-12 01:45:06 +0800

    Update ProductImplementation.java

--- src/main/java/sg/edu/iss/ims/service/ProductImplementation.java
+++ src/main/java/sg/edu/iss/ims/service/ProductImplementation.java
@@ -5,13 +5,38 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import sg.edu.iss.ims.model.Product;
 import sg.edu.iss.ims.repo.ProductRepository;
 
 @Service
 @Transactional
-public class ProductImplementation {
+public class ProductImplementation implements ProductInterface {
 	
 	@Autowired
 	private ProductRepository productRepo;
+
+	@Override
+	public void createProduct(Product product) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void readProduct(String productName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateProduct(Product product) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteProduct(Product product) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
