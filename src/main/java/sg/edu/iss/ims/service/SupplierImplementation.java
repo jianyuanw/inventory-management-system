4e0a955053258fd1dcec0118e0f62f32206979e8
MokZF404 <54626382+MokZF404@users.noreply.github.com>
2020-12-12 01:45:10 +0800

    Update SupplierImplementation.java

--- src/main/java/sg/edu/iss/ims/service/SupplierImplementation.java
+++ src/main/java/sg/edu/iss/ims/service/SupplierImplementation.java
@@ -5,13 +5,38 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import sg.edu.iss.ims.model.Supplier;
 import sg.edu.iss.ims.repo.SupplierRepository;
 
 @Service
 @Transactional
-public class SupplierImplementation {
+public class SupplierImplementation implements SupplierInterface{
 
 	@Autowired
 	private SupplierRepository supplierRepo;
+
+	@Override
+	public void createSupplier(Supplier supplier) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void readSupplier(String supplierName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateSupplier(Supplier supplier) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteSupplier(Supplier supplier) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
