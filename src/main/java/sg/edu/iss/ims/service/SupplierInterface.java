f2dba877dbb452de7039bc2998cc011843a77392
MokZF404 <54626382+MokZF404@users.noreply.github.com>
2020-12-12 01:45:13 +0800

    Update SupplierInterface.java

--- src/main/java/sg/edu/iss/ims/service/SupplierInterface.java
+++ src/main/java/sg/edu/iss/ims/service/SupplierInterface.java
@@ -1,5 +1,14 @@
 package sg.edu.iss.ims.service;
 
+import sg.edu.iss.ims.model.Supplier;
+
 public interface SupplierInterface {
 
+	public void createSupplier(Supplier supplier);
+	
+	public void readSupplier(String supplierName);
+	
+	public void updateSupplier(Supplier supplier);
+	
+	public void deleteSupplier(Supplier supplier);
 }
