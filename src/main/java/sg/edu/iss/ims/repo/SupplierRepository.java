e960d3b060649cc36e7cc9c09c0856141e5a2188
MokZF404 <54626382+MokZF404@users.noreply.github.com>
2020-12-13 04:15:17 +0800

    Update SupplierRepository.java

--- src/main/java/sg/edu/iss/ims/repo/SupplierRepository.java
+++ src/main/java/sg/edu/iss/ims/repo/SupplierRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import sg.edu.iss.ims.model.Supplier;
 
 public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+	public Supplier findSupplierByName(String name);
 
 }
