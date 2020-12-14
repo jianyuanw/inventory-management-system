e9e49f12289b5f93b77e286974c23269003d8550
Cai Yaolong Derek <caiylderek@gmail.com>
2020-12-14 23:41:02 +0800

    Urgent push to fix bugs

--- src/main/java/sg/edu/iss/ims/controller/JobController.java
+++ src/main/java/sg/edu/iss/ims/controller/JobController.java
@@ -1,6 +1,7 @@
 package sg.edu.iss.ims.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import sg.edu.iss.ims.model.Job;
@@ -8,6 +9,7 @@ import sg.edu.iss.ims.model.Job;
 @Controller
 public class JobController {
 
+	@GetMapping("/forms/supplierform")
 	public String generateSupplierForm(@ModelAttribute Job job) {
 		return "supplierform";
 	}
--- src/main/java/sg/edu/iss/ims/repo/ItemRepository.java
+++ src/main/java/sg/edu/iss/ims/repo/ItemRepository.java
@@ -1,16 +1,13 @@
 package sg.edu.iss.ims.repo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import sg.edu.iss.ims.model.Item;
-import sg.edu.iss.ims.model.Supplier;
-
 import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import sg.edu.iss.ims.model.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
-    public ArrayList<Item> findAllByUnits();
+    public ArrayList<Item> findAllByUnits(int units);
 
-  // must add here otherwise cannot be called by itemrepo object
-    void delete(int unit);
-    void save (int unit);
 }
