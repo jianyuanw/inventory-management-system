3a17ed2d7718da806698b0d10215b4718c76f0d1
MokZF404 <54626382+MokZF404@users.noreply.github.com>
2020-12-14 16:38:57 +0800

    Update SupplierService.java

--- src/main/java/sg/edu/iss/ims/service/SupplierService.java
+++ src/main/java/sg/edu/iss/ims/service/SupplierService.java
@@ -1,17 +1,18 @@
 package sg.edu.iss.ims.service;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import sg.edu.iss.ims.model.Supplier;
 
 public interface SupplierService {
 
-	public void createSupplier(Supplier supplier);
+	public void saveSupplier(Supplier supplier);
 	
-	public Supplier readSupplier(String supplierName);
+	public ArrayList<Supplier> findSupplierByName(String supplierName);
 	
-	public Supplier updateSupplier(Long id);
+	public Supplier findSupplierById(Long id);
 	
 	public void deleteSupplier(Long id);
 	
