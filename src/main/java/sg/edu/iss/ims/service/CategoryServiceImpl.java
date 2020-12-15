326227fc1e004ecbb7e3c00c854668bc76c740d7
MokZF404 <54626382+MokZF404@users.noreply.github.com>
2020-12-15 14:01:48 +0800

    Update CategoryServiceImpl.java

--- src/main/java/sg/edu/iss/ims/service/CategoryServiceImpl.java
+++ src/main/java/sg/edu/iss/ims/service/CategoryServiceImpl.java
@@ -2,13 +2,22 @@ package sg.edu.iss.ims.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import sg.edu.iss.ims.model.Category;
 import sg.edu.iss.ims.model.Subcategory;
 import sg.edu.iss.ims.repo.CategoryRepository;
 import sg.edu.iss.ims.repo.SubcategoryRepository;
 
+@Service
+@Transactional
 public class CategoryServiceImpl implements CategoryService {
+	@Autowired
 	private CategoryRepository catRepo;
+	@Autowired
 	private SubcategoryRepository subcatRepo;
 	
 	public CategoryServiceImpl(CategoryRepository catRepo, SubcategoryRepository subcatRepo) {
@@ -43,8 +52,14 @@ public class CategoryServiceImpl implements CategoryService {
 		subcatRepo.save(subcategory);
 	}
 	
+	@Override
 	public List<Subcategory> getSubcategories() {
 		return subcatRepo.findAll();
+	}
+	
+	@Override
+	public List<Category> getCategories(){
+		return catRepo.findAll();
 	}
 
 }
