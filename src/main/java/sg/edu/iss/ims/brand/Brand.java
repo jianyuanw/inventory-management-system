37fc1f683e530c5246e793e9f3221f6fec426499
Cai Yaolong Derek <caiylderek@gmail.com>
2020-12-17 13:21:04 +0800

    Slight validation and format improving

--- src/main/java/sg/edu/iss/ims/brand/Brand.java
+++ src/main/java/sg/edu/iss/ims/brand/Brand.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,8 @@ public class Brand {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotNull
 	private String name;
 
 	public Brand(String name) {
