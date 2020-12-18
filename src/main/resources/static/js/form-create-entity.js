eb3d676da40bbb1449bd2e06b25641e3152dec3d
MokZF404 <54626382+MokZF404@users.noreply.github.com>
2020-12-18 23:20:33 +0800

    Update form-create-entity.js

--- src/main/resources/static/js/form-create-entity.js
+++ src/main/resources/static/js/form-create-entity.js
@@ -1,7 +1,8 @@
 function createSelect(element) {
 	let prefix = element.id.substring(0, element.id.length - 6);
+	var selector = document.getElementById(prefix+"Select");
 	let newElement = document.getElementById(prefix + "New");
-	if (element.value == "0") {		
+	if (selector.options[selector.options.length - 1].selected == true) {		
 		newElement.classList.remove("d-none");
 	} else {
 		newElement.classList.add("d-none");
