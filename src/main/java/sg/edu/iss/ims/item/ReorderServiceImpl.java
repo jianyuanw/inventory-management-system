2f113a2b5296d88aaae46e1b40e0980e22830b7f
jianyuanw <69199572+jianyuanw@users.noreply.github.com>
2020-12-19 23:51:32 +0800

    Generated report text in required format. Added logic to write report text into a temp file "report.dat" and push as file download to user upon clicking download button.

--- src/main/java/sg/edu/iss/ims/item/ReorderServiceImpl.java
+++ src/main/java/sg/edu/iss/ims/item/ReorderServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sg.edu.iss.ims.product.Product;
 import sg.edu.iss.ims.product.ProductRepository;
+import sg.edu.iss.ims.supplier.Supplier;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -93,5 +94,47 @@ public class ReorderServiceImpl implements ReorderService {
         }
     }
 
+    @Override
+    public String generateReport(List<Reorder> reorders, Supplier supplier, double totalPrice) {
+        String strTotalPrice = String.format("%.2f", totalPrice);
 
+        String line1 = "\t\t     Inventory Reorder Report for products from Supplier " + supplier.getId() + "\n";
+        String line2 = "\t\t     ------------------------------------------------------\n";
+        String line3 = "================================================================================================\n";
+        String line4 = "Part No.\tUnit.Price\tQty\tReorder Qty.\tMin.Ord.Qty\tOrd.Qty\t\tPrice\n";
+        String line5 = "================================================================================================\n";
+
+        String line6 = "================================================================================================\n";
+        String line7 = "\t\t\t\t\t\t\t\t\t\tTOTAL\t" + strTotalPrice + "\n";
+        String line8 = "================================================================================================\n";
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(line1);
+        sb.append(line2);
+        sb.append(line3);
+        sb.append(line4);
+        sb.append(line5);
+
+        for (Reorder reorder : reorders) {
+            String partNo = reorder.getItem().getProduct().getPartNumber();
+            double unitPrice = reorder.getItem().getProduct().getOriginalPrice();
+            String strUnitPrice = String.format("%.2f", unitPrice);
+            String qty = String.valueOf(reorder.getItem().getUnits());
+            String reorderQty = String.valueOf(reorder.getItem().getReorderAt());
+            String minOrdQty = String.valueOf(reorder.getItem().getReorderQuantity());
+            int ordQty = reorder.getQuantity();
+            String strOrdQty = String.valueOf(ordQty);
+            String price = String.format("%.2f", unitPrice * ordQty);
+
+            sb.append(partNo + "\t" + strUnitPrice + "\t\t" + qty + "\t" + reorderQty + "\t\t" +
+                    minOrdQty + "\t\t" + strOrdQty + "\t\t" + price + "\n");
+        }
+
+        sb.append(line6);
+        sb.append(line7);
+        sb.append(line8);
+
+        return sb.toString();
+    }
 }
