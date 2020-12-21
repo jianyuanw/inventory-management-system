1cc0890f0928146ec89005c7a32e6dc27e8ba6ea
jianyuanw <69199572+jianyuanw@users.noreply.github.com>
2020-12-22 00:02:59 +0800

    Properly disabled register. Missed out in previous commit.

--- src/main/java/sg/edu/iss/ims/user/RegisterController.java
+++ src/main/java/sg/edu/iss/ims/user/RegisterController.java
@@ -1,41 +1,41 @@
-package sg.edu.iss.ims.user;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import sg.edu.iss.ims.model.Alert;
-
-@Controller
-public class RegisterController {
-
-	private final UserService uService;
-	
-	public RegisterController(UserServiceImpl uImp) {
-		uService = uImp;
-	}
-	
-	@GetMapping("/register")
-	public String register(Model model) {
-		model.addAttribute("user", new User());
-		return "register";
-	}
-
-	@PostMapping("/register")
-	public String createAccount(Model model, User user, HttpServletRequest request, RedirectAttributes redirAttr) {
-		HttpSession session = request.getSession();
-		if (session.getAttribute("user") == null && uService.readUser(user.getUsername()) == null) {
-			user.setPassword(uService.encode(user.getPassword()));
-			uService.createUser(user);
-			session.setAttribute("user", user);
-			redirAttr.addFlashAttribute("alert", new Alert("success", "Successfully registered!"));
-		}
-		return "redirect:/";
-	}
-
-}
+//package sg.edu.iss.ims.user;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpSession;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+//
+//import sg.edu.iss.ims.model.Alert;
+//
+//@Controller
+//public class RegisterController {
+//
+//	private final UserService uService;
+//
+//	public RegisterController(UserServiceImpl uImp) {
+//		uService = uImp;
+//	}
+//
+//	@GetMapping("/register")
+//	public String register(Model model) {
+//		model.addAttribute("user", new User());
+//		return "register";
+//	}
+//
+//	@PostMapping("/register")
+//	public String createAccount(Model model, User user, HttpServletRequest request, RedirectAttributes redirAttr) {
+//		HttpSession session = request.getSession();
+//		if (session.getAttribute("user") == null && uService.readUser(user.getUsername()) == null) {
+//			user.setPassword(uService.encode(user.getPassword()));
+//			uService.createUser(user);
+//			session.setAttribute("user", user);
+//			redirAttr.addFlashAttribute("alert", new Alert("success", "Successfully registered!"));
+//		}
+//		return "redirect:/";
+//	}
+//
+//}
