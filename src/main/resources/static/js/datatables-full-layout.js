9dc62cd76f357d27a8fa13552ea18b2508da5d5b
MokZF404 <54626382+MokZF404@users.noreply.github.com>
2020-12-22 16:06:04 +0800

    Made product list datatable scrollable

--- src/main/resources/static/js/datatables-full-layout.js
+++ src/main/resources/static/js/datatables-full-layout.js
@@ -2,3 +2,10 @@ $(document).ready(function() {
 	$('#dataTable').DataTable();
 	$('.dataTables_length').addClass('bs-select');
 });
+
+$(document).ready(function() {
+	$('#dataTableH').DataTable({
+	"scrollX": true});
+	$('.dataTables_length').addClass('bs-select');
+});
+
